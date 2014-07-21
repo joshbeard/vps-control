81aba778e8f1ae35693e38f8d2308d1addc8dbf9
Josh Beard <beard@puppetlabs.com>
2014-07-20 22:19:43 -0600

    Resolve firewall dependencies

--- site/profile/manifests/firewall.pp
+++ site/profile/manifests/firewall.pp
@@ -1,28 +1,24 @@
 class profile::firewall {
-  ## Make sure pre and post happens in the right order
-  Firewall {
-    before  => Class['profile::firewall::post'],
-    require => Class['profile::firewall::pre'],
-  }
-
   ## Include the puppetlabs-firewall class
   class { '::firewall': }
 
   ## Our pre/post rules
-  class { '::profile::firewall::pre': }
+  class { '::profile::firewall::pre': }->
   class { '::profile::firewall::post': }
 
   ## Now our rules
   firewall { '003 accept ssh':
-    proto  => 'tcp',
-    action => 'accept',
-    port   => '22431',
+    proto   => 'tcp',
+    action  => 'accept',
+    port    => '22431',
+    require => Class['::profile::firewall::pre'],
   }
 
   firewall { '004 accept http':
     proto  => 'tcp',
     action => 'accept',
     port   => ['80','443'],
+    require => Class['::profile::firewall::pre'],
   }
 
 }
