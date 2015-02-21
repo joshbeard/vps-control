d3c3c6e426916c422d4c19b7ad1399b69c5b21d5
Josh Beard <beard@puppetlabs.com>
2015-02-21 15:04:25 -0700

    Syntax error - missing comma

--- site/profile/manifests/nginx_server.pp
+++ site/profile/manifests/nginx_server.pp
@@ -11,7 +11,7 @@ class profile::nginx_server {
   if $::osfamily == 'FreeBSD' {
     file { '/etc/newsyslog.conf.d/nginx.conf':
       ensure  => 'file',
-      content => '/var/log/nginx/*.log    644     12      *       $M1D0   JGC             /var/run/nginx.pid'
+      content => '/var/log/nginx/*.log    644     12      *       $M1D0   JGC             /var/run/nginx.pid',
       owner   => 'root',
       group   => 'wheel',
       mode    => '0640',
