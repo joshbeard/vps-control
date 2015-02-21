8639d052ff5ba2e5b2ce8cbc82f0aea971700f23
Josh Beard <beard@puppetlabs.com>
2015-02-21 15:03:16 -0700

    Provide nginx log rotation on FreeBSD

--- site/profile/manifests/nginx_server.pp
+++ site/profile/manifests/nginx_server.pp
@@ -7,4 +7,14 @@ class profile::nginx_server {
   file { '/var/log/nginx':
     ensure => 'directory',
   }
+
+  if $::osfamily == 'FreeBSD' {
+    file { '/etc/newsyslog.conf.d/nginx.conf':
+      ensure  => 'file',
+      content => '/var/log/nginx/*.log    644     12      *       $M1D0   JGC             /var/run/nginx.pid'
+      owner   => 'root',
+      group   => 'wheel',
+      mode    => '0640',
+    }
+  }
 }
