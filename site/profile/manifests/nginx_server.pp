37cdcb6de721c01602985445bed1840c97b8c18c
Josh Beard <beard@puppetlabs.com>
2015-02-21 03:38:06 -0700

    Disable nginx server tokens

--- site/profile/manifests/nginx_server.pp
+++ site/profile/manifests/nginx_server.pp
@@ -1,6 +1,8 @@
 ## The nginx profile
 class profile::nginx_server {
-  class { '::nginx': }
+  class { '::nginx':
+    server_tokens => 'off',
+  }
 
   file { '/var/log/nginx':
     ensure => 'directory',
