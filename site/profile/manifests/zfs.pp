33c8f53abf5564c57acee1a825481b7192565317
Josh Beard <beard@puppetlabs.com>
2015-02-21 18:53:57 -0700

    Zpool fixes

--- site/profile/manifests/zfs.pp
+++ site/profile/manifests/zfs.pp
@@ -2,7 +2,12 @@ class profile::zfs {
 
   zpool { 'store':
     ensure => 'present',
-    mirror => [ 'ada1', 'ada2' ],
+    mirror => 'ada1 ada2',
+  }
+
+  file { '/store/media':
+    ensure  => 'directory',
+    require => Zpool['store'],
   }
 
 }
