efb82406ddd87623befdbde1f95225e4a44f4941
Josh Beard <beard@puppetlabs.com>
2015-02-21 20:14:57 -0700

    Update cron schedule for freebsd updates

--- site/profile/manifests/base/freebsd.pp
+++ site/profile/manifests/base/freebsd.pp
@@ -4,18 +4,21 @@ class profile::base::freebsd {
     ensure  => 'present',
     command => 'freebsd-update cron',
     hour    => '2',
+    minute  => '15',
   }
 
   cron { 'ports_update':
     ensure  => 'present',
     command => 'portsnap cron && portsnap update',
     hour    => '3',
+    minute  => '15',
   }
 
   cron { 'pkg_update':
     ensure  => 'present',
     command => 'pkg update && pkg version -vRL=',
     hour    => '4',
+    minute  => '0',
   }
 
 }
