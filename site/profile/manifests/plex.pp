e99cdfef2f8e222c075e33bc4976254ebf611ce4
Josh Beard <beard@puppetlabs.com>
2015-02-21 18:51:14 -0700

    Use proper plex paths on FreeBSD

--- site/profile/manifests/plex.pp
+++ site/profile/manifests/plex.pp
@@ -1,29 +1,36 @@
 class profile::plex {
-  $plugin_dir = "/usr/local/share/plexmediaserver/Resources/Plug-ins"
+  $base_dir   = '/usr/local/plexdata/Plex Media Server'
+  $plugin_dir = '/usr/local/plexdata/Plex Media Server/Plug-ins'
 
   package { 'plexmediaserver':
     ensure => 'installed',
   }
 
-  service { 'plexmediaserver':
-    ensure => 'running',
-    enable => true,
-  }
-
-  file { 'Plex_Plugin_dir':
+  file { [ $base_dir, $plugin_dir ]:
     ensure  => 'directory',
-    path    => "/usr/local/share/plexmediaserver/Resources/Plug-ins",
+    owner   => 'plex',
+    group   => 'plex',
+    require => Package['plexmediaserver'],
   }
 
   staging::deploy { 'Plex_Unsupported_App_store.zip':
     source  => 'http://bit.ly/ihqmEu',
     target  => $plugin_dir,
     creates => "${plugin_dir}/UnSupportedAppstore.bundle",
+    require => File[$plugin_dir],
+    notify  => Service['plexmediaserver'],
   }
 
   vcsrepo { "${plugin_dir}/LetMeWatchThis.bundle":
     ensure   => 'present',
     provider => 'git',
     source   => 'https://github.com/ReallyFuzzy/LetMeWatchThis.bundle.git',
+    require  => File[$plugin_dir],
+    notify   => Service['plexmediaserver'],
+  }
+
+  service { 'plexmediaserver':
+    ensure => 'running',
+    enable => true,
   }
 }
