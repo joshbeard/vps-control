b7e0d51f6b78821627e883c1f8bb2524d496dc00
Josh Beard <beard@puppetlabs.com>
2015-03-03 21:04:37 -0700

    Add minecraft profile

---
+++ site/profile/manifests/minecraft.pp
@@ -0,0 +1,11 @@
+class profile::minecraft {
+
+  include mcmyadmin
+
+  mcmyadmin::instance { 'default':
+    webserver_port    => '8080',
+    user              => 'minecraft',
+    group             => 'minecraft',
+  }
+
+}
