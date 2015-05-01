5ca989c13256eb9cfd70255896b3fcdf20402d3b
Josh Beard <josh@signalboxes.net>
2015-04-30 23:05:15 -0600

    Make sure pf is available before updating rules

--- site/profile/manifests/firewall/freebsd.pp
+++ site/profile/manifests/firewall/freebsd.pp
@@ -1,12 +1,14 @@
 class profile::firewall::freebsd {
-  class { 'pf':
-    template => "${module_name}/pf.erb",
-  }
-
   service { 'pf':
     ensure  => 'running',
     enable  => true,
-    require => Class['pf'],
+  }
+
+  # The /dev/pf device needs to exist for 'pfctl update' to work. The service
+  # provides that.
+  class { 'pf':
+    template => "${module_name}/pf.erb",
+    require  => Service['pf'],
   }
 
   service { 'pflog':
