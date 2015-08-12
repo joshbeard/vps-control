361fdccd965e12ff79093c6b5c7d57548bbf597e
Josh Beard <josh@signalboxes.net>
2015-08-11 21:51:42 -0600

    Update hiera config

--- site/profile/manifests/puppet.pp
+++ site/profile/manifests/puppet.pp
@@ -2,25 +2,6 @@
 ## Profile for configuring the Puppet agent and master on the vps
 ##
 class profile::puppet inherits profile::params {
-  Ini_setting {
-    ensure  => 'present',
-    path    => "${::settings::confdir}/puppet.conf",
-    section => 'main',
-  }
-
-  File {
-    owner => 'root',
-    group => '0',
-  }
-
-  ## For hiera-eyaml
-  $hiera_eyaml_config = [
-    ':eyaml:',
-    "  :datadir: ${::settings::environmentpath}/%{environment}/hieradata",
-    "  :pkcs7_private_key: ${::settings::confdir}/keys/private_key.pkcs7.pem",
-    "  :pkcs7_public_key: ${::settings::confdir}/keys/public_key.pkcs7.pem",
-    '  :extension: "yaml"',
-  ]
 
   ## Static entry in /etc/hosts
   host { $::fqdn:
@@ -45,31 +26,6 @@ class profile::puppet inherits profile::params {
     mcollective       => false,
   }
 
-  ini_setting { 'certname':
-    setting => 'certname',
-    value   => $::fqdn,
-  }
-
-  ini_setting { 'server':
-    setting => 'server',
-    value   => $::fqdn,
-  }
-
-  ## Manage hiera-eyaml's key directory
-  file { 'eyaml_keys':
-    ensure => 'directory',
-    path   => "${::settings::confdir}/keys",
-    owner  => 'root',
-    group  => '0',
-    mode   => '0600',
-  }
-
-  ## Install hiera-eyaml via Rubygems
-  package { 'hiera-eyaml':
-    ensure   => 'installed',
-    provider => 'gem',
-  }
-
   ## Configure Hiera
   class { 'hiera':
     hierarchy => [
@@ -77,9 +33,8 @@ class profile::puppet inherits profile::params {
       '%{environment}',
       'common',
     ],
+    eyaml        => true,
     datadir      => "${::settings::environmentpath}/%{environment}/hieradata",
-    backends     => ['eyaml', 'yaml'],
-    extra_config => join($hiera_eyaml_config, "\n"),
     owner        => 'root',
     group        => 'root',
   }
