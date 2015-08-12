63a79551d717cfd377f12e28910aaba1716ed8c6
Josh Beard <josh@signalboxes.net>
2015-08-11 21:28:37 -0600

    Update signalboxes.net git url

--- site/profile/manifests/signalboxes.pp
+++ site/profile/manifests/signalboxes.pp
@@ -7,7 +7,7 @@ class profile::signalboxes {
   vcsrepo { '/var/www/signalboxes.net':
     ensure   => 'present',
     provider => 'git',
-    source   => 'https://gitlab.com/joshbeard/signalboxes.net.git',
+    source   => 'https://gitlab.com/joshbeard/signalboxes-net.git',
     group    => 'web',
     require  => File['/var/www'],
   }
