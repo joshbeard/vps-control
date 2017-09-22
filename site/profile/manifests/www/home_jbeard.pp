063582f342abdd16b4b4ca1d495f7f5eb1e3d41f
Josh Beard <jbeard@usgs.gov>
2017-09-22 10:44:23 -0600

    venus adjustments

--- site/profile/manifests/www/home_jbeard.pp
+++ site/profile/manifests/www/home_jbeard.pp
@@ -5,18 +5,4 @@ class profile::www::home_jbeard {
     notify => Class['::nginx'],
   }
 
-  openssl::certificate::x509 { 'home.jbeard.org':
-    ensure       => 'present',
-    country      => 'US',
-    organization => 'jbeard.org',
-    commonname   => 'home.jbeard.org',
-    email        => 'josh@signalboxes.net',
-    days         => '3456',
-    base_dir     => "${::nginx::params::conf_dir}/ssl",
-    owner        => $::nginx::params::daemon_user,
-    group        => $::nginx::params::root_group,
-    require      => Class['::nginx::package'],
-    before       => Nginx::Resource::Server['home.jbeard.org'],
-  }
-
 }
